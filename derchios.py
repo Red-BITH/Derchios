887732563c7bc92970f23795bdcbf13cbee80226
RED BİTH <136892734+Red-BITH@users.noreply.github.com>
2023-11-20 09:02:36 -0800

    Update derchios.py

--- derchios.py
+++ derchios.py
@@ -1,1 +1,65 @@
+import os
+import sys
+import time
+print("""\033[32m
+######   #######  ######    #####   ### ###   ######   #####    #####   
+ ## ###   ##  ##   ##  ##  ##   ##   ## ##      ##    ### ###  ##   ##  
+ ##  ##   ##       ##  ##  ##   ##   ## ##      ##    ##   ##  ##       
+ ##  ##   ####     #####   ##        #####      ##    ##   ##   #####   
+ ##  ##   ##       ##  ##  ##   ##   ## ##      ##    ##   ##       ##  
+ ## ###   ##  ##   ##  ##  ##   ##   ## ##      ##    ### ###  ##   ##  
+######   #######  #### ###  #####   ### ###   ######   #####    #####   
+                                                                        
+########  #####    #####   ###      
+## ## ## ### ###  ### ###   ##      
+   ##    ##   ##  ##   ##   ##      
+   ##    ##   ##  ##   ##   ##      
+   ##    ##   ##  ##   ##   ##      
+   ##    ### ###  ### ###   ##  ##  
+  ####    #####    #####   #######  
+""")
+import sys
+import time
 
+def loading_animation():
+    symbols = ["-", "|", "/", "\\"]
+    
+    for i in range(1, 16):
+        sys.stdout.write('\r')
+        sys.stdout.write('-' * i + symbols[i % len(symbols)])
+        sys.stdout.flush()
+        time.sleep(0.2)
+
+print("\033[35m\nYüləmələr yoxlanılır:")
+loading_animation()
+print("\033[32m\nYüləmələr yoxlanıldı: ok!")
+os.system("clear")
+print("""\033[33mSalam dəyərli istifadəçi! DERCHİOS toolunu istifadə etdiyiniz üçün təşəkkürlər!
+Bir öncəki Red Tool proyektimdə Multiplatforma xüsusiyyəti var idi.və zaman alan bir boot sistemi var idi.
+Burada isə Platformaya görə kod ayrımına az yer verilib.Və mümkünlük çərçivəsində bütün xüsusiyyətlər 
+termux\linux\cloud shell və s. də çalışacaq.""")
+import sys
+import time
+
+import sys
+import time
+
+class TerminalColors:
+    BLUE = "\033[34m"
+    RESET = "\033[0m"
+def loading_animation2():
+    symbols = ["|", "/","~", "\\"]
+    
+    for _ in range(3):
+        for symbol in symbols:
+            sys.stdout.write('\r' + TerminalColors.BLUE + symbol + TerminalColors.RESET)
+            sys.stdout.flush()
+            time.sleep(0.4)
+
+loading_animation2()
+os.system("clear")
+
+
+
+
+                                    
