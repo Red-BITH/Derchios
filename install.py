5c309d487e7e765c7b3e94934aabb278625f3708
RED BİTH <136892734+Red-BITH@users.noreply.github.com>
2023-11-20 08:22:55 -0800

    Update install.py

--- install.py
+++ install.py
@@ -1,1 +1,54 @@
-s
+import os
+os.system("clear")
+print("""\033[31m\[*creator--RedBith----*]
+Salam! Siz Derchios Tool-un 
+Qurulum ekranındasınız! Qurulum Auto gerçəkləşdirilir. 
+README.md faylını oxuduğunuz sayılır.
+[*]---Davam etmək üçün hər hansıbir şey yazın və enterə basın!
+""")
+txt = input("--->")
+if(txt == "1234567890" or "A aB bC cD dE eF fG gH hI iJ jK kL lM mN nO oP pQ qR rS sT tU uV vW wX xY yZ z"):
+  os.system("clear")
+  os.system("pip install pygame")
+  os.system("pip install requests")
+  os.system("pip3 install termcolor")
+  os.system("pip3 install tabulate")
+  os.system("apt install python3")
+  os.system("apt install dmitry")
+  os.system("apt install nmap")
+  os.system("python -m pip install --upgrade pip")
+  os.system("python -m pip install --upgrade termcolor")
+  os.system("clear")
+
+else:
+  os.system("clear")
+  os.system("pip install pygame")
+  os.system("pip install requests")
+  os.system("pip3 install termcolor")
+  os.system("pip3 install tabulate")
+  os.system("apt install python3")
+  os.system("apt install dmitry")
+  os.system("apt install nmap")
+  os.system("python -m pip install --upgrade pip")
+  os.system("python -m pip install --upgrade termcolor")
+  os.system("clear")
+import time
+print("""print("\033[32m/n Əsas Qurulum tamamlandı! Artıq Toolu işə salmaq üçün təlimatları yerinə yetirin:
+[*]Qurulum bitti. Klaviaturadan hər hansı bir şey yazıb ENTER basın və qurulumu bitirin.(bu proqramı dayadırın)
+[*]python Derchios.py (toolu işə salın)
+{Hazırladı---RedBith}
+""")
+a = input("--->")
+if(a == "1234567890" or "A aB bC cD dE eF fG gH hI iJ jK kL lM mN nO oP pQ qR rS sT tU uV vW wX xY yZ z"):
+  print("ok!")
+else:
+  print("ok!")
+  
+
+
+
+
+
+
+
+
