f9064960aeee91e24dac1c8fc4fad8d3e0df2240
RED BİTH <136892734+Red-BITH@users.noreply.github.com>
2023-12-01 04:20:39 -0800

    Update derchios.py

--- derchios.py
+++ derchios.py
@@ -89,7 +89,8 @@ print("""\033[34m
 Öncəliklə Derchiosu təcrübə etdiyiniz üçün təşəkkürlər!""")
 print("""\033[33mChange:(rəqəm)
 1 ---> Site adress\Ip port-scan\scan
-2 --->
+2 ---> Telefon Nomresi Scan
+3 --->
 """)
 tool = input("--->")
 if(tool == "1"):
@@ -105,9 +106,42 @@ if(tool == "1"):
  import os
  os.system("nmap -sC -sV -sS" + ip")
 
-           
+if(tool == "2"):
+ import phonenumbers
+ from phonenumbers import carrier, timezone, geocoder
+ 
+ def clear_console():
+  import os
+  if os.name == 'posix':
+   os.system('clear')
+  elif os.name == 'nt':
+   os.system('cls')
+ 
+ def main():
+  clear_console()
+  print("CyberDark.Org & Rwoolx")
+  number = input("Telefon nömrəsini girin:")
+  if number.startswith("+"):
+   try:
+    parsed_number = phonenumbers.parse(number, None)
+    if phonenumbers.is_valid_number(parsed_number):
+     print("Yazılan nömrə doğrudur.")
+     time_zones = timezone.time_zones_for_number(parsed_number)
+     carrier_name = carrier.name_for_number(parsed_number, "az")
+     region = geocoder.description_for_number(parsed_number, "az")
+     print("Sim Şəbəkəsi:", carrier_name)
+     print("Nömrənin Ölkəsi:", region)
+     print("Nömrənin Bölgəsi:", time_zones)
 
+    else:
+     print("Yazılan nömrə səhvdir!")
+   except phonenumbers.phonenumberutil.NumberFormatException:
+    print("Xətalı nömrə formatı.")
+  else:
+   print("+Ölkə kodunu yazmaği unutdunuz!.")
 
+ if __name__ == "__main__":
+  main()
 
 
 
